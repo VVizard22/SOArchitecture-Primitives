2ebce7dac4b2fd40df56bdcc14331f280404e5e5
Dante Iván García <61606117+VVizard22@users.noreply.github.com>
2023-02-06 20:18:16 -0300

    Initial Commit

---
+++ Primitives/Variables/BoolVariable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace SOArchitecture.Variables
+{
+    [CreateAssetMenu]
+    public class BoolVariable : ScriptableObject
+    {
+        [SerializeField]
+        private bool value = false;
+
+        public bool Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+}
