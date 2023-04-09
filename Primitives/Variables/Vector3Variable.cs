cd841282f9313cbefe12184f685de50124e82f0b
Dante Iván García <61606117+VVizard22@users.noreply.github.com>
2023-04-08 23:51:14 -0300

    Added Vector3 Variables
    Added a new SO class with respective reference type:
    - Vector3Variable
    - Vector3Reference

---
+++ Primitives/Variables/Vector3Variable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOArchitecture.Variables
+{
+    [CreateAssetMenu(menuName = "SOVariables/Vector2Variable")]
+    public class Vector3Variable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+
+        public Vector3 Value;
+
+        public void SetValue(Vector3 value) => Value = value;
+        public void SetValue(Vector3Reference value) => Value = value.Value;
+
+        public void ApplyChanges(Vector3 amount) => Value += amount;
+
+        public void ApplyChanges(Vector3Reference amount) => Value += amount.Value;
+    }
+}
